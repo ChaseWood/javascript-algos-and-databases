1a10346005c4a2939cdac4f1c4f02a76d27684ff
Chase Wood <chase_wood07@yahoo.com>
2021-01-05 10:27:19 -0600

    updating global email

--- commonPatterns/multiplePointers/isSubsequence.js
+++ commonPatterns/multiplePointers/isSubsequence.js
@@ -29,4 +29,3 @@ const isSubsequence = (str1, str2) => {
 console.log(isSubsequence('hello', 'hello world'));
 console.log(isSubsequence('sing', 'sting'));
 console.log(isSubsequence('abc', 'abracadabra'));
-console.log(isSubsequence('abc', 'acb'));
