4c69dcf8850d4bbc15cdfc96886d59ad72a47d25
Chase Wood <chase_wood07@yahoo.com>
2021-01-07 10:09:04 -0600

    cleaned up bubbleSort

--- sortingAlgorithms/bubbleSort.js
+++ sortingAlgorithms/bubbleSort.js
@@ -1,25 +1,4 @@
-// // Bubble Sort
-
-// const bubbleSort = (arr) => {
-// 	let noSwaps;
-// 	for (let i = arr.length; i > 0; i--) {
-// 		noSwaps = true;
-// 		for (let j = 0; j < i - 1; j++) {
-// 			if (arr[j] > arr[j + 1]) {
-// 				let temp = arr[j];
-// 				arr[j] = arr[j + 1];
-// 				arr[j + 1] = temp;
-// 				noSwaps = false;
-// 			}
-// 			if (noSwaps) break;
-// 		}
-// 	}
-// 	return arr;
-// };
-
-// console.log(bubbleSort([8, 1, 2, 3, 4, 6, 4, 10]));
-
-// Optimized BubbleSort with noSwaps
+//BubbleSort with noSwaps
 function bubbleSort(arr) {
 	var noSwaps;
 	for (var i = arr.length; i > 0; i--) {
