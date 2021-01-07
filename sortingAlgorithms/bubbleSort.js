b8786840f16b229e61094319e7e036f7d9f1372d
Chase Wood <chase_wood07@yahoo.com>
2021-01-07 10:11:35 -0600

    updated bubbleSort syntax

--- sortingAlgorithms/bubbleSort.js
+++ sortingAlgorithms/bubbleSort.js
@@ -1,11 +1,11 @@
 //BubbleSort with noSwaps
-function bubbleSort(arr) {
-	var noSwaps;
-	for (var i = arr.length; i > 0; i--) {
+const bubbleSort = (arr) => {
+	let noSwaps;
+	for (let i = arr.length; i > 0; i--) {
 		noSwaps = true;
-		for (var j = 0; j < i - 1; j++) {
+		for (let j = 0; j < i - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
-				var temp = arr[j];
+				let temp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
 				noSwaps = false;
@@ -14,6 +14,6 @@ function bubbleSort(arr) {
 		if (noSwaps) break;
 	}
 	return arr;
-}
+};
 
 console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
